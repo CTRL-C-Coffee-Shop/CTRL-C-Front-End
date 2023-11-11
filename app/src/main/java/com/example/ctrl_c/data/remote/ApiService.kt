52484ecdef8888778688c5f9bd8b20d60bee166c
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-11 18:28:33 +0700

    Creating API Config and API Service

---
+++ app/src/main/java/com/example/ctrl_c/data/remote/ApiService.kt
@@ -0,0 +1,19 @@
+package com.example.ctrl_c.data.remote
+
+import com.example.ctrl_c.model.response.GeneralResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("/register")
+    suspend fun register(
+        @Header("apiKey") apiKey: String,
+        @Field("FullName") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("AccessType") accessType: Int,
+    ): GeneralResponse
+}

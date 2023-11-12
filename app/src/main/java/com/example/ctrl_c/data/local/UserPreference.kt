fa42128b56ba316b22fa4e9bcee27828b31f616a
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-12 12:12:11 +0700

    creating user preference to save token

---
+++ app/src/main/java/com/example/ctrl_c/data/local/UserPreference.kt
@@ -0,0 +1,21 @@
+package com.example.ctrl_c.data.local
+
+import android.content.Context
+
+class UserPreference(context: Context) {
+    private val preferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+
+    fun getToken(): String? {
+        return preferences.getString(TOKEN, null)
+    }
+
+    fun saveToken(token: String) {
+        val edit = preferences.edit()
+        edit.putString(TOKEN, token)
+        edit.apply()
+    }
+
+    companion object {
+        private const val TOKEN = "token"
+    }
+}

b4548cd5331ba046f88eb5898bd2b5b2b9f952aa
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-21 15:22:18 +0700

    Create ProductViewModel.kt

---
+++ app/src/main/java/com/example/ctrl_c/viewmodel/product/ProductViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ctrl_c.viewmodel.product
+
+import androidx.lifecycle.ViewModel
+import com.example.ctrl_c.data.repository.Repository
+
+class ProductViewModel(private val repository: Repository): ViewModel() {
+    fun getAllProduct() = repository.getProduct()
+}

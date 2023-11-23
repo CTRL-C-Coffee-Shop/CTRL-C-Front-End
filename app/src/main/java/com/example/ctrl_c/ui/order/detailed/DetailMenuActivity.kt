5ef5a44fba978bfa50b8e56fce5e5b98ddd8adeb
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-23 22:27:00 +0700

    changing string to english

--- app/src/main/java/com/example/ctrl_c/ui/order/detailed/DetailMenuActivity.kt
+++ app/src/main/java/com/example/ctrl_c/ui/order/detailed/DetailMenuActivity.kt
@@ -1,12 +1,26 @@
 package com.example.ctrl_c.ui.order.detailed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ctrl_c.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ctrl_c.databinding.ActivityDetailMenuBinding
+import com.example.ctrl_c.helper.LoadingHandler
 
-class DetailMenuActivity : AppCompatActivity() {
+class DetailMenuActivity : AppCompatActivity(), LoadingHandler {
+
+    private lateinit var binding: ActivityDetailMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_menu)
+        binding = ActivityDetailMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+
+    override fun loadingHandler(isLoading: Boolean) {
+        if (isLoading) {
+            binding.loadingAnimation.visibility = View.VISIBLE
+        } else {
+            binding.loadingAnimation.visibility = View.GONE
+        }
     }
 }

17c3ecee23a137dacfa8d95ba8c26dd476fbd76e
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-24 09:14:30 +0700

    adding increase ammount and decrease ammount function

--- app/src/main/java/com/example/ctrl_c/ui/order/detailed/DetailMenuActivity.kt
+++ app/src/main/java/com/example/ctrl_c/ui/order/detailed/DetailMenuActivity.kt
@@ -2,7 +2,6 @@ package com.example.ctrl_c.ui.order.detailed
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.ctrl_c.R
@@ -13,15 +12,20 @@ import com.example.ctrl_c.model.response.product.ProductItem
 class DetailMenuActivity : AppCompatActivity(), LoadingHandler {
 
     private lateinit var binding: ActivityDetailMenuBinding
+    var productAmmount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
         setupDetailedProduct()
+        updateAmmount()
+        setupAction()
     }
-    
+
     private fun setupDetailedProduct() {
         val product = intent.getParcelableExtra<ProductItem>("product")
         if (product != null) {
@@ -33,6 +37,33 @@ class DetailMenuActivity : AppCompatActivity(), LoadingHandler {
                     .load(product.url)
                     .error(R.drawable.default_menu)
                     .into(imageView2)
+            }
+        }
+    }
+
+    private fun increaseAmmount() {
+        productAmmount++
+    }
+
+    private fun decreaseAmmount() {
+        if (productAmmount > 0) {
+            productAmmount--
+        }
+    }
+
+    private fun updateAmmount() {
+        binding.textView12.text = productAmmount.toString()
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            materialButton9.setOnClickListener {
+                decreaseAmmount()
+                updateAmmount()
+            }
+            materialButton10.setOnClickListener {
+                increaseAmmount()
+                updateAmmount()
             }
         }
     }

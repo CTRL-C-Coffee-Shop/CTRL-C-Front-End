ce893361bb559560943d87dd63348ec1e67c05b8
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-28 23:49:54 +0700

    Fixing Notification Manager

--- app/src/main/java/com/example/ctrl_c/helper/ReminderWorker.kt
+++ app/src/main/java/com/example/ctrl_c/helper/ReminderWorker.kt
@@ -6,48 +6,72 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import java.util.concurrent.TimeUnit
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.ctrl_c.R
 
 
-class ReminderWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams)  {
+class ReminderWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
 
     companion object {
         const val NOTIFICATION_ID = 1
         const val CHANNEL_ID = "channel_01"
-        const val CHANNEL_NAME = "dicoding channel"
+        const val CHANNEL_NAME = "CTRL+C channel"
     }
 
-    private val sharedPreferences: SharedPreferences =
-        context.getSharedPreferences("notification_pref", Context.MODE_PRIVATE)
-
+//    private val sharedPreferences: SharedPreferences =
+//        context.getSharedPreferences("notification_pref", Context.MODE_PRIVATE)
 
     private var resultStatus: Result? = null
 
+//    override fun doWork(): Result {
+//
+//        val lastNotificationTime = sharedPreferences.getLong("last_notification_time", 0)
+//        val currentTime = System.currentTimeMillis()
+//
+//        //Changeable Timestamp
+//        if (currentTime - lastNotificationTime >= TimeUnit.SECONDS.toMillis(5)) {
+
+//            showNotification(title, desc)
+//
+//            with(sharedPreferences.edit()) {
+//                putLong("last_notification_time", currentTime)
+//                apply()
+//            }
+//        }
+//        return Result.success()
+//    }
+
+
     override fun doWork(): Result {
-
-        val lastNotificationTime = sharedPreferences.getLong("last_notification_time", 0)
-        val currentTime = System.currentTimeMillis()
-
-        //Changeable Timestamp
-        if (currentTime - lastNotificationTime >= TimeUnit.SECONDS.toMillis(5)) {
+        // Check inactivity logic here
+        if (isInactiveFor2Minutes(applicationContext)) {
             val title = applicationContext.getString(R.string.notification_title)
             val desc = applicationContext.getString(R.string.notification_message)
             showNotification(title, desc)
-
-            with(sharedPreferences.edit()) {
-                putLong("last_notification_time", currentTime)
-                apply()
-            }
         }
         return Result.success()
     }
 
-    private fun showNotification(title: String, description: String?) : Result {
+    private fun isInactiveFor2Minutes(context: Context): Boolean {
+        val sharedPreferences = context.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        val lastActiveTime = sharedPreferences.getLong(
+            "lastActiveTime",
+            0L
+        ) // Get last active time from SharedPreferences
+
+        val currentTime = System.currentTimeMillis()
+        val elapsedTime = currentTime - lastActiveTime
+
+        // Check if elapsed time is more than 2 seconds (2,000 milliseconds)
+        return elapsedTime >= (1 * 1000 * 60)
+    }
+
+    private fun showNotification(title: String, description: String?): Result {
         resultStatus = Result.failure()
-        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setSmallIcon(R.drawable.baseline_notifications_24)
             .setContentTitle(title)
@@ -55,7 +79,8 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) : Worker(
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            val channel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
             notification.setChannelId(CHANNEL_ID)
             notificationManager.createNotificationChannel(channel)
             resultStatus = Result.success()
@@ -63,4 +88,5 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) : Worker(
         notificationManager.notify(NOTIFICATION_ID, notification.build())
         return resultStatus as Result
     }
+
 }
--- app/src/main/java/com/example/ctrl_c/ui/onBoarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/ctrl_c/ui/onBoarding/OnBoardingActivity.kt
@@ -1,17 +1,15 @@
 package com.example.ctrl_c.ui.onBoarding
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import java.util.concurrent.TimeUnit
 import com.example.ctrl_c.R
 import com.example.ctrl_c.data.local.UserPreference
 import com.example.ctrl_c.databinding.ItemOnBoardingBinding
-import com.example.ctrl_c.helper.ReminderWorker
 import com.example.ctrl_c.ui.admin.AdminPageActivity
 import com.example.ctrl_c.ui.authentication.login.LoginActivity
 import com.example.ctrl_c.ui.main.MainActivity
@@ -27,7 +25,7 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ItemOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-
+        requestNotificationPermission()
         checkToken()
         setupAction()
     }
@@ -93,10 +91,9 @@ class OnBoardingActivity : AppCompatActivity() {
         //checking if user already logged in or not.
         val pref = UserPreference(this)
         val token = pref.getToken()
-        val userTyoe = pref.getUserType()
+        val userType = pref.getUserType()
         if (token != null) {
-            if (!userTyoe) {
-                startApplicationReminder()
+            if (!userType) {
                 navigateToMainActivity()
             } else {
                 navigateToAdminActivity()
@@ -110,14 +107,22 @@ class OnBoardingActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun startApplicationReminder() {
-        val workRequest = OneTimeWorkRequestBuilder<ReminderWorker>()
-            //Changeable Timestamp
-            .setInitialDelay(5, TimeUnit.SECONDS)
-            .build()
 
-        WorkManager.getInstance(this).enqueue(workRequest)
+    private fun requestNotificationPermission() {
+        // Minta izin notifikasi menggunakan launcher yang telah didaftarkan
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     companion object {
         const val MAX_STEP = 2

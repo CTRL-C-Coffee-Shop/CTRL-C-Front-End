4502128470d94f6fb83cb76d936e76e42f0ae7ce
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-11-28 22:35:45 +0700

    adding getTotalPriceFunction

--- app/src/main/java/com/example/ctrl_c/ui/order/checkout/adapter/OrderCheckoutAdapter.kt
+++ app/src/main/java/com/example/ctrl_c/ui/order/checkout/adapter/OrderCheckoutAdapter.kt
@@ -15,23 +15,23 @@ class OrderCheckoutAdapter : RecyclerView.Adapter<OrderCheckoutAdapter.ListViewH
     private var currentList: List<CartItem> = emptyList()
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvName: TextView = itemView.findViewById(R.id.tv_product_name_order_checkout)
-        val tvDesc: TextView = itemView.findViewById(R.id.tv_product_desc_order_checkout)
-        val tvPrice: TextView = itemView.findViewById(R.id.tv_product_price_order_checkout)
-        val tvAmount: TextView = itemView.findViewById(R.id.tv_product_amount_order_checkout)
-        val ivImage: ImageView = itemView.findViewById(R.id.tv_product_image_order_checkout)
+        val tvName: TextView = itemView.findViewById(R.id.tv_product_name_order_checkout_cart)
+        val tvDesc: TextView = itemView.findViewById(R.id.tv_product_desc_order_checkout_cart)
+        val tvPrice: TextView = itemView.findViewById(R.id.tv_product_price_order_checkout_cart)
+        val tvAmount: TextView = itemView.findViewById(R.id.tv_product_amount_order_checkout_cart)
+        val ivImage: ImageView = itemView.findViewById(R.id.tv_product_image_order_checkout_cart)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val productName = currentList[position].product.name
         val productDesc = currentList[position].product.description
-        val productPrice = currentList[position].product.price
+        val productPrice = currentList[position].product.price * currentList[position].amount
         val productAmount = currentList[position].amount
 
         holder.tvName.text = productName
         holder.tvDesc.text = productDesc
-        holder.tvPrice.text = productPrice.toString()
-        holder.tvAmount.text = productAmount.toString()
+        holder.tvPrice.text = "Rp. $productPrice.000"
+        holder.tvAmount.text = "${productAmount}x"
 
         Glide.with(holder.itemView.context)
             .load(currentList[position].product.url)
@@ -40,12 +40,21 @@ class OrderCheckoutAdapter : RecyclerView.Adapter<OrderCheckoutAdapter.ListViewH
 
     }
 
+    fun getTotalPrice(): Int {
+        var totalPrice = 0
+        for (cartItem in currentList) {
+            val productPrice = cartItem.product.price * cartItem.amount
+            totalPrice += productPrice
+        }
+        return totalPrice
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ListViewHolder {
         val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.menu_list, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_order_checkout, parent, false)
         return ListViewHolder(view)
     }
 

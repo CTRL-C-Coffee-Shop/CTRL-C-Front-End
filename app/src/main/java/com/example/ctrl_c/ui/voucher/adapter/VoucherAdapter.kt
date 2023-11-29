91b59385fcb7fec9a39f81d1455e79dd19b4b11a
ignatiusdariel <73424643+ignatiusdariel@users.noreply.github.com>
2023-11-29 12:51:50 +0700

    VoucherList UI Done and Adapter Init
    VoucherList xml Done and Adapter Initiated

---
+++ app/src/main/java/com/example/ctrl_c/ui/voucher/adapter/VoucherAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.ctrl_c.ui.voucher.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class VoucherAdapter: RecyclerView.Adapter<VoucherAdapter.ListViewHolder>()  {
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
